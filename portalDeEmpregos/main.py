69651738c8c9e4678a4a5abdd7b75eb35fde60a4
Roniclelton-Br <ronicleltonbr@gmail.com>
2024-09-16 10:46:15 -0300

    responsividade homepage e iniciado login.html

--- portalDeEmpregos/main.py
+++ portalDeEmpregos/main.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def homepage():
     return render_template('homepage.html')
 
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
