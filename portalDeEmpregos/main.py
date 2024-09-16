ddd799369a13e8b873451a01f1af0297ee6acd02
Ricardo H. <henrique.sousas100@gmail.com>
2024-09-16 16:28:50 -0300

    host 0.0.0.0

--- portalDeEmpregos/main.py
+++ portalDeEmpregos/main.py
@@ -9,8 +9,9 @@ def homepage():
 
 @app.route('/login')
 def login():
+    url_for('static', filename='stylesLogin.css')
     return render_template('login.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host = '0.0.0.0', port = 5500)
