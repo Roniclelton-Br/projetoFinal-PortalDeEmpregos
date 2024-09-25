fd96c3f69727308531f6f1a812860358ade78fb8
Roniclelton <ronicleltonbr@gmail.com>
2024-09-25 11:27:20 -0300

    update vagas, empresas, candidatos, aplicacao

--- portalDeEmpregos/main.py
+++ portalDeEmpregos/main.py
@@ -59,28 +59,35 @@ def homepage():
                 # Ver vagas
                 conn.execute("SELECT * FROM vagas")
                 resultado_vagas = conn.fetchall()
+                contador = 1
 
                 for vaga in resultado_vagas:
-                    info_vagas = f'<li class="ver">{vaga[2]}</li>'  # Ajuste o índice conforme necessário
+
+                    info_vagas = f'''<li class="ver" onclick="mostrarDiv('div_id{contador}')">{vaga[2]}<div id = "div_id{contador}"><ul><li><br>ID: {vaga[0]}<br>CNPJ: {vaga[2]}<br>EMPRESA: {vaga[1]}<br>FUNÇÃO: {vaga[3]}<br>SALÁRIO: R${vaga[4]:.2F}</li></ul></div></li>'''  # Ajuste o índice conforme necessário
                     vagas.append(info_vagas)
+                    contador += 1
               
                 #ver empresas
                 conn.execute("SELECT * FROM empresa")
                 resultado_empresas = conn.fetchall()
+                
 
                 for empresa in resultado_empresas:
-                    info_empresas = f'<li class="ver">{empresa[1]}</li>'  # Ajuste o índice conforme necessário
+                    info_empresas = f'''<li class="ver" onclick="mostrarDiv('div_id{contador}')">{empresa[1]}<div id = "div_id{contador}"><ul><li><br>ID: {empresa[0]}<br>CNPJ: {empresa[2]}<br>LOCALIZAÇÃO: {empresa[3]}<br>PORTE: {empresa[4]}<br>DESCRIÇÃO: {empresa[5]}</li></ul></div></li>'''  # Ajuste o índice conforme necessário
                     empresas.append(info_empresas)
+                    contador += 1
 
 
                 
                 #ver candidatos
                 conn.execute("SELECT * FROM candidato")
                 resultado_candidato = conn.fetchall()
+                
 
                 for candidato in resultado_candidato:
-                    info_candidato = f'<li class="ver">{candidato[1]}<br><br>ID: {candidato[0]}<br>CPF: {candidato[2]}<br>TELEFONE: {candidato[3]}<br>ENDEREÇO: {candidato[4]}</li>'  # Ajuste o índice conforme necessário
+                    info_candidato = f'''<li class="ver" onclick="mostrarDiv('div_id{contador}')">{candidato[1]}<div id = "div_id{contador}"><ul><li><br>ID: {candidato[0]}<br>CPF: {candidato[2]}<br>TELEFONE: {candidato[3]}<br>ENDEREÇO: {candidato[4]}</li></ul></div></li>'''  # Ajuste o índice conforme necessário
                     candidatos.append(info_candidato)
+                    contador += 1
 
 
 
@@ -89,7 +96,7 @@ def homepage():
                 resultado_aplicacao = conn.fetchall()
 
                 for aplicacao in resultado_aplicacao:
-                    info_aplicacao = f'<li class="ver">ID: {aplicacao[0]} </li>'  # Ajuste o índice conforme necessário
+                    info_aplicacao = f'<li class="ver">ID: {aplicacao} </li>'  # Ajuste o índice conforme necessário
                     aplicacoes.append(info_aplicacao)
 
 
