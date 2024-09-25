f67c3ef77d5d24a2fc534a449b477f71a805d751
Ricardo H. <henrique.sousas100@gmail.com>
2024-09-25 10:14:53 -0300

    corrigindo o erro do banco de dados

--- portalDeEmpregos/main.py
+++ portalDeEmpregos/main.py
@@ -1,82 +1,81 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import mysql.connector
-from mysql.connector import Error
+import pymysql
 import logging
 
 app = Flask(__name__)
 app.secret_key = 'my_secret_key'
-
-logging.basicConfig(level=logging.ERROR)  # Configuração de registro
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     texto = ''
     if request.method == 'POST':
         try:
-            with mysql.connector.connect(
+            conexao_bd = pymysql.connect(
                 host='localhost',
                 user='root',
                 password='mysql',
-                database='portaldeempregos',
-                auth_plugin='mysql_native_password'
-            ) as conexao_bd:
-                with conexao_bd.cursor() as conn:
-                    username = request.form['username']
-                    password = request.form['password']
+                database='portaldeempregos'
+            )
+            with conexao_bd.cursor() as conn:
+                username = request.form['username']
+                password = request.form['password']
 
-                    conn.execute('SELECT * FROM adm WHERE username_adm = %s', (username,))
-                    usuario = conn.fetchone()
+                conn.execute('SELECT * FROM adm WHERE username_adm = %s', (username,))
+                usuario = conn.fetchone()
 
-                    if usuario:
-                        if password == usuario[-1]:  # Comparando a senha em texto puro
-                            session['username'] = usuario[1]
-                            return redirect(url_for('homepage'))
-                        else:
-                            texto = 'Senha incorreta'
+                if usuario:
+                    if password == usuario[-1]:  # Comparando a senha em texto puro
+                        session['username'] = usuario[1]
+                        return redirect(url_for('homepage'))
                     else:
-                        texto = 'Usuário incorreto'
-        except Error as e:
-            logging.error(f'Erro de conexão: {e}')
+                        texto = 'Senha incorreta'
+                else:
+                    texto = 'Usuário incorreto'
+        except pymysql.MySQLError as e:
+            logging.error(f'Erro ao conectar ao banco de dados: {e}')
             texto = 'Erro ao conectar ao banco de dados.'
+        finally:
+            if conexao_bd:
+                conexao_bd.close()
     return render_template('login.html', texto=texto)
 
 @app.route('/index')
 def homepage():
-    vagas = []  # Lista para armazenar as vagas
+    vagas = []
     empresas = []
 
     try:
-        with mysql.connector.connect(
+        conexao_bd = pymysql.connect(
             host='localhost',
             user='root',
             password='mysql',
-            database='portaldeempregos',
-            auth_plugin='mysql_native_password'
-        ) as conexao_bd:
-            with conexao_bd.cursor() as conn:
-                # Ver vagas
-                conn.execute("SELECT * FROM vagas")
-                resultado_vagas = conn.fetchall()
+            database='portaldeempregos'
+        )
+        with conexao_bd.cursor() as conn:
+            # Ver vagas
+            conn.execute("SELECT * FROM vagas")
+            resultado_vagas = conn.fetchall()
 
-                for vaga in resultado_vagas:
-                    info_vagas = f'<li class="ver">{vaga[2]}</li>'  # Ajuste o índice conforme necessário
-                    vagas.append(info_vagas)
+            for vaga in resultado_vagas:
+                info_vagas = f'<li class="ver">{vaga[2]}</li>'
+                vagas.append(info_vagas)
               
-                #ver empresas
-                conn.execute("SELECT * FROM empresa")
-                resultado_empresas = conn.fetchall()
+            # Ver empresas
+            conn.execute("SELECT * FROM empresa")
+            resultado_empresas = conn.fetchall()
 
-                for empresa in resultado_empresas:
-                    info_empresas = f'<li class="ver">{empresa[1]}</li>'  # Ajuste o índice conforme necessário
-                    empresas.append(info_empresas)
-                   
-    except Error as erro:
-        logging.error(f'Erro ao buscar vagas: {erro}')
+            for empresa in resultado_empresas:
+                info_empresas = f'<li class="ver">{empresa[1]}</li>'
+                empresas.append(info_empresas)
+    except pymysql.MySQLError as erro:
+        logging.error(f'Erro ao buscar dados: {erro}')
         vagas = ['Erro ao buscar vagas.']
         empresas = ['Erro ao buscar empresas.']
+    finally:
+        if conexao_bd:
+            conexao_bd.close()
 
-    return render_template('index.html', username=session.get('username'), vagas_ver=vagas,  empresas_ver=empresas)
-
+    return render_template('index.html', username=session.get('username'), vagas_ver=vagas, empresas_ver=empresas)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5500)
