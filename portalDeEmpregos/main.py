3ee8d43677337e7fa288aaa5d18eae0381e38dfc
Roniclelton Brito <ronicleltonbr@gmail.com>
2024-09-28 14:52:34 -0300

    criado as funções de adicionar

--- portalDeEmpregos/main.py
+++ portalDeEmpregos/main.py
@@ -2,11 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import mysql.connector
 from mysql.connector import Error
 import logging
-from datetime import datetime
 app = Flask(__name__)
 app.secret_key = 'my_secret_key'
 
-logging.basicConfig(level=logging.ERROR)  # Configuração de registro
+logging.basicConfig(level=logging.ERROR) 
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -28,7 +27,7 @@ def login():
                     usuario = conn.fetchone()
 
                     if usuario:
-                        if password == usuario[-1]:  # Comparando a senha em texto puro
+                        if password == usuario[-1]: 
                             session['username'] = usuario[1]
                             return redirect(url_for('homepage'))
                         else:
@@ -42,7 +41,7 @@ def login():
 
 @app.route('/index')
 def homepage():
-    vagas = []  # Lista para armazenar as vagas
+    vagas = []  
     empresas = []
     candidatos = []
     aplicacoes = []
@@ -57,7 +56,7 @@ def homepage():
         ) as conexao_bd:
             with conexao_bd.cursor() as conn:
                 # Ver vagas
-                conn.execute("SELECT * FROM vagas WHERE  status_vaga = 'ATIVO'")
+                conn.execute("SELECT * FROM vagas WHERE  status_vaga = 'ATIVO' ORDER BY nome_vaga ASC;")
 
                 resultado_vagas = conn.fetchall()
                 contador = 1
@@ -75,7 +74,7 @@ def homepage():
                     </li>
                     <ul class="forms">
                     <li>
-                    <form action="{ url_for('delete_vaga', vaga_id = vaga[0]) }" method="POST" style="display:inline;">
+                    <form style="display:inline;">
                     <button type="submit">Editar</button>
                     </form>
                     </li>
@@ -92,7 +91,7 @@ def homepage():
                     contador += 1
 
                 #ver empresas
-                conn.execute("SELECT * FROM empresa  WHERE status_empresa = 'ATIVO'")
+                conn.execute("SELECT * FROM empresa  WHERE status_empresa = 'ATIVO' ORDER BY nome_empresa ASC;")
 
                 resultado_empresas = conn.fetchall()
                 
@@ -101,7 +100,7 @@ def homepage():
                     info_empresas = f'''<li class="ver" onclick="mostrarDiv('div_id{contador}')">{empresa[1]}<div id = "div_id{contador}"><ul><li><br>ID: {empresa[0]}<br>CNPJ: {empresa[2]}<br>LOCALIZAÇÃO: {empresa[3]}<br>PORTE: {empresa[4]}<br>DESCRIÇÃO: {empresa[5]}<br>STATUS: {empresa[6]}</li></ul>
                     <ul class="forms">
                     <li>
-                    <form action="{ url_for('delete_vaga', vaga_id = vaga[0]) }" method="POST" style="display:inline;">
+                    <form style="display:inline;">
                     <button type="submit">Editar</button>
                     </form>
                     </li>
@@ -117,7 +116,7 @@ def homepage():
 
                 
                 #ver candidatos
-                conn.execute("SELECT * FROM candidato WHERE status_candidato = 'ATIVO'")
+                conn.execute("SELECT * FROM candidato WHERE status_candidato = 'ATIVO' ORDER BY nome_candidato ASC;")
 
                 resultado_candidato = conn.fetchall()
                 
@@ -126,7 +125,7 @@ def homepage():
                     info_candidato = f'''<li class="ver" onclick="mostrarDiv('div_id{contador}')">{candidato[1]}<div id = "div_id{contador}"><ul><li><br>ID: {candidato[0]}<br>CPF: {candidato[2]}<br>TELEFONE: {candidato[3]}<br>ENDEREÇO: {candidato[4]}<br>E-MAIL: {candidato[5]}<br>STATUS: {candidato[6]}</li></ul>
                     <ul class="forms">
                     <li>
-                    <form action="{ url_for('delete_candidato', candidato_id = candidato[0]) }" method="POST" style="display:inline;">
+                    <form style="display:inline;">
                     <button type="submit">Editar</button>
                     </form>
                     </li>
@@ -142,7 +141,7 @@ def homepage():
 
 
                 #ver aplicações
-                conn.execute("SELECT * FROM aplicacao WHERE status_aplicacao = 'ATIVO'")
+                conn.execute("SELECT * FROM aplicacao WHERE status_aplicacao = 'ATIVO' ORDER BY data_aplicacao ASC;")
                 resultado_aplicacao = conn.fetchall()
 
                 for aplicacao in resultado_aplicacao:
@@ -153,10 +152,10 @@ def homepage():
                     nome_candidato = conn.fetchone()
 
 
-                    info_aplicacao = f'''<li class="ver" onclick="mostrarDiv('div_id{contador}')">VAGA: {nome_vaga[0]}: CANDIDATO: {nome_candidato[0]}<div id = "div_id{contador}"><ul><li><br>ID: {aplicacao[0]}<br>ID CANDIDATO: {aplicacao[1]}<br>ID VAGA: {aplicacao[2]}<br>DATA APLICAÇÃO: {aplicacao[3]}<br>STATUS: {aplicacao[4]}</li></ul>
+                    info_aplicacao = f'''<li class="ver" onclick="mostrarDiv('div_id{contador}')">VAGA: {nome_vaga[0]} | CANDIDATO: {nome_candidato[0]}<div id = "div_id{contador}"><ul><li><br>ID: {aplicacao[0]}<br>ID CANDIDATO: {aplicacao[1]}<br>ID VAGA: {aplicacao[2]}<br>DATA APLICAÇÃO: {aplicacao[3]}<br>STATUS: {aplicacao[4]}</li></ul>
                     <ul class="forms">
                     <li>
-                    <form action="{ url_for('delete_aplicacao', aplicacao_id = aplicacao[0]) }" method="POST" style="display:inline;">
+                    <form style="display:inline;">
                     <button type="submit">Editar</button>
                     </form>
                     </li>
@@ -266,6 +265,108 @@ def delete_aplicacao(aplicacao_id):
     return redirect(url_for('homepage'))
 
 
+
+
+
+
+@app.route('/add_candidato', methods=['GET', 'POST'])
+def add_candidato():
+    try:
+        with mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='mysql',
+            database='portaldeempregos',
+            auth_plugin='mysql_native_password'
+        ) as conexao_bd:
+            with conexao_bd.cursor() as conn:
+                if request.method == 'POST':
+
+                    nome = request.form.get('nome_candidato')
+                    cpf = request.form.get('cpf_candidato')
+                    telefone = request.form.get('telefone_candidato', '00000000000')
+                    endereco = request.form.get('endereco_candidato', 'SEM ENDEREÇO')
+                    email = request.form.get('email_candidato', 'SEM E-MAIL')
+
+                    sql_adicionar = """
+        INSERT INTO candidato (nome_candidato, cpf_candidato, telefone_candidato, endereco_candidato, email_candidato)
+        VALUES (%s, %s, %s, %s, %s)
+        """
+                conn.execute(sql_adicionar, (nome, cpf, telefone, endereco, email))
+                conexao_bd.commit()
+                
+    except Error as e:
+        logging.error(f'Erro ao deletar vaga: {e}')
+    return redirect(url_for('homepage'))
+
+
+
+
+
+
+
+@app.route('/add_empresa', methods=['GET', 'POST'])
+def add_empresa():
+    try:
+        with mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='mysql',
+            database='portaldeempregos',
+            auth_plugin='mysql_native_password'
+        ) as conexao_bd:
+            with conexao_bd.cursor() as conn:
+                if request.method == 'POST':
+                    nome_empresa = request.form['nome_empresa']
+                    cnpj_empresa = request.form['cnpj_empresa']
+                    localizacao_empresa = request.form['localizacao_empresa']
+                    porte_empresa = request.form['porte_empresa']
+                    descricao_empresa = request.form['descricao_empresa']
+
+                    sql_adicionar = """INSERT INTO empresa (nome_empresa, cnpj_empresa, localizacao_empresa, 
+                    porte_empresa, descricao_empresa) 
+                    VALUES (%s, %s, %s, %s, %s)"""
+
+                    conn.execute(sql_adicionar, (nome_empresa, cnpj_empresa, localizacao_empresa, porte_empresa, descricao_empresa)
+                    )
+                conexao_bd.commit()
+                
+    except Error as e:
+        logging.error(f'Erro ao deletar vaga: {e}')
+    return redirect(url_for('homepage'))
+
+
+
+
+
+
+@app.route('/add_vaga', methods=['GET', 'POST'])
+def add_vaga():
+    try:
+        with mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='mysql',
+            database='portaldeempregos',
+            auth_plugin='mysql_native_password'
+        ) as conexao_bd:
+            with conexao_bd.cursor() as conn:
+                if request.method == 'POST':
+                    id_empresa = request.form['id_empresa']
+                    nome_vaga = request.form['nome_vaga']
+                    funcao_vaga = request.form['funcao_vaga']
+                    salario_vaga = request.form['salario_vaga']
+
+                    sql_adicionar = """INSERT INTO vagas (id_empresa, nome_vaga, funcao_vaga, salario_vaga) 
+                    VALUES (%s, %s, %s, %s)"""
+                    
+                    conn.execute(sql_adicionar, (id_empresa, nome_vaga, funcao_vaga, salario_vaga)
+                    )
+                    conexao_bd.commit()
+                
+    except Error as e:
+        logging.error(f'Erro ao deletar vaga: {e}')
+    return redirect(url_for('homepage'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5500)
