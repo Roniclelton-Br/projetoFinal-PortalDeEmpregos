91d9d3ebb50c7855c0d1b6a66569cba85904817a
Ricardo H. <henrique.sousas100@gmail.com>
2024-10-05 21:48:23 -0300

    rick

--- portalDeEmpregos_usuario/app/app.py
+++ portalDeEmpregos_usuario/app/app.py
@@ -1,10 +1,10 @@
 import flask
 
-app = flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route("/")
 def homePage():
     return flask.render_template('homepage.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
