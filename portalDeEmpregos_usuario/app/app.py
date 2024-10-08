9245d734fa4bf006fb9824c396f47fe6d3b061d7
Ricardo H. <henrique.sousas100@gmail.com>
2024-10-08 15:09:25 -0300

    update user

--- portalDeEmpregos_usuario/app/app.py
+++ portalDeEmpregos_usuario/app/app.py
@@ -11,5 +11,8 @@ def login():
 @app.route("/signup")
 def register():
     return flask.render_template('register_user.html')
+@app.route("/contatos")
+def contato():
+    return flask.render_template('contato.html')
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
