adf26a826989ce3f868ed2e37b83702760a9c60e
Ricardo H. <henrique.sousas100@gmail.com>
2024-10-07 23:37:32 -0300

    atualização

--- portalDeEmpregos_usuario/app/app.py
+++ portalDeEmpregos_usuario/app/app.py
@@ -5,6 +5,9 @@ app = flask.Flask(__name__)
 @app.route("/")
 def homePage():
     return flask.render_template('homepage.html')
+@app.route("/login")
+def login():
+    return flask.render_template('login_user.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
