3c260f3980ab5ef8fd24d260e48083ee03815084
Ricardo H. <henrique.sousas100@gmail.com>
2024-11-10 20:20:12 -0300

    update

--- portalDeEmpregos_usuario/app/teste_routas.py
+++ portalDeEmpregos_usuario/app/teste_routas.py
@@ -5,29 +5,46 @@ app =  Flask(__name__)
 @app.route("/")
 
 def teste():
-    return main_pg(id_user = 1)
+    return main_pg(name = str('ricardo'))
 
 @app.route("/main_pg")
-def main_pg( id_user = int):
+def main_pg( name = str):
 
-    user = ID(id_user)
+    user = ID(name)
     data = user.get_id()
-    print(data)
+    #print(data)
+    code = None
 
     if data[1]['account_type'] == 'candidato':
-        code = f''
-        return render_template("main.html", name = data[0]['nome'], code = code)
+        if code is None:
+            print("erro")
+            codeErro = """
+            <div class='erro-data'>
+                <p>Nenhuma vaga registrada até o momento.
+                </p>
+            </div>"""
+            print ('state-3')
+            return render_template("main.html", name = data[0]['nome'].upper(), code = codeErro)
+
+        print ('state-2')
+        return render_template("main.html", name = data[0]['nome'].upper(), code = code)
 
         
-
-    return render_template("main.html", name = data[0]['nome'])
+    print ('state-1')
+    return render_template("main.html", name = data[0]['nome'].upper())
 
 class ID():
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, name):
+        self.name = name 
 
     def get_id(self):
-        return [{'nome': 'Ricardo'}, {'account_type': 'candidato'}]
+        return [{'nome': 'Ricardo'}, {'account_type': 'candidato'}, {'ID': '0'}]
+    
+    def search_bd(self):
+        pass
+
+    def edit_bd(self):
+        pass
 
 if __name__ == "__main__":
 
